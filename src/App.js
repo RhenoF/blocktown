df39abad9fb3643c273b97ec886915a7d07c43be
Rheno Formiga <rheno.a.formiga.neto@avanade.com>
2021-05-17 11:42:38 -0300

    working implementation of blockly drawer

--- src/App.js
+++ src/App.js
@@ -1,58 +1,98 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import React, { useState, useRef } from "react";
+import { useSelector, useDispatch } from 'react-redux';
+import Blockly from "node-blockly/browser";
+import "./App.css";
+import {
+  setCount,
+  selectCount,
+} from './features/counter/counterSlice';
+import BlocklyDrawer, { Block, Category } from "react-blockly-drawer";
+
+const helloWorld = {
+  name: "HelloWorld",
+  category: "Demo",
+  block: {
+    init: function () {
+      this.jsonInit({
+        message0: "Hello %1",
+        args0: [
+          {
+            type: "field_input",
+            name: "NAME",
+            check: "String",
+          },
+        ],
+        output: "String",
+        colour: 160,
+        tooltip: "Says Hello",
+      });
+    },
+  },
+  generator: (block) => {
+    const message = `'${block.getFieldValue("NAME")}'` || "''";
+    const code = `console.log('Hello ${message}')`;
+    return [code, Blockly.JavaScript.ORDER_MEMBER];
+  },
+};
+
+function Content() {
+
+  const count = useSelector(selectCount);
+
+  return(
+  <p className="row">{count}</p>
+  );
+}
+
+function Drawer(props) {
+
+
+  const dispatch = useDispatch();
+  const workspaceChange = (code, workspace) => {
+    console.log(code, workspace);
+    dispatch(setCount(code))
+  };
+
+
+  return (
+    <BlocklyDrawer
+      className="row"
+      tools={[helloWorld]}
+      onChange={workspaceChange}
+      language={Blockly.JavaScript}
+      injectOptions={{
+        css: true,
+        scrollbars: false,
+        grid: { spacing: 20, length: 3, colour: "#ccc", snap: true },
+      }}
+      appearance={{
+        categories: {
+          Demo: {
+            colour: "270",
+          },
+        },
+      }}
+    >
+      <Category name="Variables" custom="VARIABLE" />
+      <Category name="Values">
+        <Block type="controls_for" />
+        <Block type="controls_if" />
+        <Block type="logic_compare" />
+        <Block type="math_arithmetic" />
+        <Block type="math_number" />
+        <Block type="text" />
+      </Category>
+    </BlocklyDrawer>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <Drawer />
+      <Content />
     </div>
-  );
+  );;;
 }
 
 export default App;
